8e73df562fbb4b14faf64fa46c6835666afd6ebe
Dmitry Shaygorodskii <lih41@mail.ru>
2024-05-09 18:12:25 +0500

    second

---
+++ ex01/ft_ultimate_ft.c
@@ -0,0 +1,28 @@
+void	ft_ultimate_ft(int *********nbr)
+{
+	*********nbr = 42;
+}
+
+/*int	main(void)
+{
+	int *nbr;
+	int **nbrh;
+	int ***nbrg;
+	int ****nbrf;
+	int *****nbre;
+	int ******nbrd;
+	int *******nbrc;
+	int ********nbrb;
+	int *********nbra;
+	int zalupa;
+	nbra = &nbrb;
+	nbrb = &nbrc;
+	nbrc = &nbrd;
+	nbrd = &nbre;
+	nbre = &nbrf;
+	nbrf = &nbrg;
+	nbrg = &nbrh;
+	nbrh = &nbr;
+	nbr = &zalupa;
+	ft_ultimate_ft (nbra);
+}*/
